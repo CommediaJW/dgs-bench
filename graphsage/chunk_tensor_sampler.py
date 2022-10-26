7a632b85c3322a0fd07d61dce4321b0d5a6a6119
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2022-10-26 13:04:03 +0800

    use chunktensor for feat

--- graphsage/chunk_tensor_sampler.py
+++ graphsage/chunk_tensor_sampler.py
@@ -1,6 +1,7 @@
 from dgl.dataloading.base import BlockSampler
 import torch
 import dgl
+import torch.distributed as dist
 
 
 class ChunkTensorSampler(BlockSampler):
@@ -8,12 +9,11 @@ class ChunkTensorSampler(BlockSampler):
     def __init__(self,
                  fanouts,
                  g,
+                 feat_mem_used=0,
+                 model_mem_used=0,
+                 avaliable_mem=None,
                  prob=None,
                  replace=False,
-                 cache_percent_indices=0.0,
-                 cache_percent_indptr=0.0,
-                 cache_percent_probs=0.0,
-                 comm_size=1,
                  prefetch_node_feats=None,
                  prefetch_labels=None,
                  prefetch_edge_feats=None,
@@ -25,62 +25,79 @@ class ChunkTensorSampler(BlockSampler):
         self.fanouts = fanouts
         self.prob = prob
         self.replace = replace
+        self.available_mem = avaliable_mem
 
         indptr = g.adj_sparse("csc")[0]
         indices = g.adj_sparse("csc")[1]
         if self.prob:
             probs = g.edata[self.prob]
 
-        if comm_size > 1:
-            self.chunk_indptr = torch.classes.dgs_classes.ChunkTensor(
-                indptr,
-                int((indptr.numel() * indptr.element_size() *
-                     cache_percent_indptr) / comm_size) +
-                indptr.element_size())
+        if self.available_mem is None:
+            device = torch.cuda.current_device()
+            max_device_mem = torch.cuda.get_device_properties(
+                device).total_memory
+            _available_mem = torch.tensor(
+                [(max_device_mem - model_mem_used * 2 - feat_mem_used)]).long().cuda()
+            print(_available_mem)
 
-            self.chunk_indices = torch.classes.dgs_classes.ChunkTensor(
-                indices,
-                int((indices.numel() * indices.element_size() *
-                     cache_percent_indices) / comm_size) +
-                indices.element_size())
+            print(max_device_mem - model_mem_used * 2 - feat_mem_used)
 
-            if self.prob:
-                self.chunk_probs = torch.classes.dgs_classes.ChunkTensor(
-                    probs,
-                    int((probs.numel() * probs.element_size() *
-                         cache_percent_probs) / comm_size) +
-                    probs.element_size())
+            dist.all_reduce(_available_mem, dist.ReduceOp.MIN)
+            self.available_mem = _available_mem.cpu().numpy()[0]
 
-        else:  # 1 gpu
-            max_allocated_mem = torch.cuda.max_memory_allocated(device=0)
-            total_mem = torch.cuda.get_device_properties(0).total_memory
+        print("Available mem for ChunkTensor {} MB",
+              self.available_mem / 1024 / 1024)
 
-            available_mem = max(total_mem - max_allocated_mem, 0)
-            indptr_cached_size = min(indptr.numel() * indptr.element_size(),
-                                     available_mem)
+        comm_size = dist.get_world_size()
 
-            available_mem = max(available_mem - indptr_cached_size, 0)
-            indices_cached_size = min(indices.numel() * indices.element_size(),
-                                      available_mem)
+        indptr_cached_size = min(indptr.numel() * indptr.element_size() // comm_size,
+                                 self.available_mem)
+        self.chunk_indptr = torch.classes.dgs_classes.ChunkTensor(
+            indptr, indptr_cached_size)
+        self.available_mem = self.available_mem - indptr_cached_size
+        print("Cache indptr per GPU {} MB".format(
+            indptr_cached_size / 1024 / 1024))
 
-            self.chunk_indptr = torch.classes.dgs_classes.ChunkTensor(
-                indptr, indptr_cached_size)
+        if self.prob:
+            _need_mem = (indices.numel() * indices.element_size() +
+                         probs.numel() * probs.element_size()) // comm_size
+            _cached_mem = min(_need_mem, self.available_mem)
+
+            indices_cached_size = int(_cached_mem / (
+                indices.element_size() + probs.element_size()) * indices.element_size())
+            probs_cached_size = int(_cached_mem / (
+                indices.element_size() + probs.element_size()) * probs.element_size())
+
             self.chunk_indices = torch.classes.dgs_classes.ChunkTensor(
                 indices, indices_cached_size)
+            print("Cache indices per GPU {} MB".format(
+                indices_cached_size / 1024 / 1024))
 
-            if self.prob:
-                available_mem = max(available_mem - indices_cached_size, 0)
-                probs_cached_size = min(probs.numel() * probs.element_size(),
-                                        available_mem)
+            self.chunk_probs = torch.classes.dgs_classes.ChunkTensor(
+                probs, probs_cached_size)
+            print("Cache probs per GPU {} MB".format(
+                indices_cached_size / 1024 / 1024))
 
-                self.chunk_probs = torch.classes.dgs_classes.ChunkTensor(
-                    probs, probs_cached_size)
+            self.available_mem = self.available_mem - \
+                indices_cached_size - probs_cached_size
+
+        else:
+            indices_cached_size = min(indices.numel() *
+                                      indices.element_size() // comm_size, self.available_mem)
+            self.chunk_indices = torch.classes.dgs_classes.ChunkTensor(
+                indices, indices_cached_size)
+            self.available_mem = self.available_mem - indices_cached_size
+            print("Cache indices per GPU {} MB".format(
+                indices_cached_size / 1024 / 1024))
 
     def __del__(self):
         del self.chunk_indices
         del self.chunk_indptr
         if (self.prob):
             del self.chunk_probs
+
+    def get_available_mem(self):
+        return self.available_mem
 
     def sample_blocks(self, g, seed_nodes, exclude_eids=None):
         seeds = seed_nodes
