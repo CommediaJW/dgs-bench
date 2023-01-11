f0662166ddae1c22444204fd9ac0ff98da27b0b2
552388389@qq.com <552388389@qq.com>
2023-01-11 14:37:19 +0800

    update sampling modeling bench

--- modeling/sampling.py
+++ modeling/sampling.py
@@ -1,7 +1,6 @@
 import argparse
 import load_graph
 import numpy
-from ogb.nodeproppred import DglNodePropPredDataset
 import time
 import torch
 import torch.distributed as dist
@@ -22,8 +21,8 @@ def get_available_memory(device, num_node):
     return available_mem.cpu().numpy()[0]
 
 
-def bench(rank, world_size, indptr, indices, indptr_host_cache_rate,
-          indices_host_cache_rate, fan_outs, seeds_num):
+def bench(rank, world_size, indptr, indices, probs, indptr_host_cache_rate,
+          indices_host_cache_rate, probs_host_cache_rate, fan_outs, seeds_num):
     torch.cuda.set_device(rank)
     torch.manual_seed(rank)
     dist.init_process_group('nccl',
@@ -46,6 +45,16 @@ def bench(rank, world_size, indptr, indices, indptr_host_cache_rate,
             indices.element_size() // world_size))
     chunk_indices = torch.classes.dgs_classes.ChunkTensor(
         indices, indices_cache_size_per_gpu)
+    if probs:
+        avaliable_men = get_available_memory(rank, graph_node_num)
+        probs_cache_size_per_gpu = int(
+            min(avaliable_men, (1 - probs_host_cache_rate) * probs.numel() *
+                probs.element_size() // world_size))
+        chunk_probs = torch.classes.dgs_classes.ChunkTensor(
+            probs, probs_cache_size_per_gpu)
+        probs_fact_host_cache_rate = (
+            probs.numel() * probs.element_size() - world_size *
+            probs_cache_size_per_gpu) / (probs.numel() * probs.element_size())
     indptr_fact_host_cache_rate = (indptr.numel() * indptr.element_size() -
                                    world_size * indptr_cache_size_per_gpu) / (
                                        indptr.numel() * indptr.element_size())
@@ -54,68 +63,78 @@ def bench(rank, world_size, indptr, indices, indptr_host_cache_rate,
         world_size * indices_cache_size_per_gpu) / (indices.numel() *
                                                     indices.element_size())
 
-    if rank == 0:
-        print(
-            'Seeds num {} | Indptr host cache rate {:.3f} | Indices host cache rate {:.3f}'
-            .format(seeds_num, indptr_fact_host_cache_rate,
-                    indices_fact_host_cache_rate))
-
     minimum_access_node_num_log = []
-    fact_access_node_num_log = []
-    access_time_log = []
-    total_sampling_time_log = []
+    sampling_time_log = []
+    avg_seeds_degree_log = []
+    fact_seeds_num_log = []
     for _ in range(10):
         seeds = torch.randint(0, graph_node_num,
                               (seeds_num, )).unique().long().cuda()
+        fact_seeds_num = 0
         minimum_access_node_num = 0
-        fact_access_node_num = 0
-        access_time = 0
-        total_sampling_time = 0
-
+        sampling_time = 0
+        seeds_degree_log = []
         for fan_out in fan_outs:
-            torch.cuda.synchronize()
-            start = time.time()
-            coo_row, coo_col = torch.ops.dgs_ops._CAPI_sample_neighbors_with_chunk_tensor(
-                seeds, chunk_indptr, chunk_indices, fan_out, False)
-            torch.cuda.synchronize()
-            access_time += time.time() - start
+            if probs:
+                torch.cuda.synchronize()
+                start = time.time()
+                coo_row, coo_col = torch.ops.dgs_ops._CAPI_sample_neighbors_with_probs_with_chunk_tensor(
+                    seeds, chunk_indptr, chunk_indices, chunk_probs, fan_out,
+                    False)
+                torch.cuda.synchronize()
+                sampling_time += time.time() - start
+            else:
+                torch.cuda.synchronize()
+                start = time.time()
+                coo_row, coo_col = torch.ops.dgs_ops._CAPI_sample_neighbors_with_chunk_tensor(
+                    seeds, chunk_indptr, chunk_indices, fan_out, False)
+                torch.cuda.synchronize()
+                sampling_time += time.time() - start
             frontier, (coo_row,
                        coo_col) = torch.ops.dgs_ops._CAPI_tensor_relabel(
                            [seeds, coo_col], [coo_row, coo_col])
-            torch.cuda.synchronize()
-            total_sampling_time += time.time() - start
 
+            fact_seeds_num += seeds.numel()
             minimum_access_node_num += coo_row.numel()
             for seed in seeds:
-                fact_access_node_num += indptr[seed + 1] - indptr[seed]
+                seeds_degree_log.append(indptr[seed + 1] - indptr[seed])
 
             seeds = frontier
 
+        fact_seeds_num_log.append(fact_seeds_num)
         minimum_access_node_num_log.append(minimum_access_node_num)
-        fact_access_node_num_log.append(fact_access_node_num)
-        access_time_log.append(access_time)
-        total_sampling_time_log.append(total_sampling_time)
+        sampling_time_log.append(sampling_time)
+        avg_seeds_degree_log.append(numpy.mean(seeds_degree_log))
 
-    print(
-        'GPU {} | Minimum access node num {:.1f} | Fact access node num {:.1f} | Access time {:.3f} ms | Total sampling time {:.3f} ms'
-        .format(rank, numpy.mean(minimum_access_node_num_log[3:]),
-                numpy.mean(fact_access_node_num_log[3:]),
-                numpy.mean(access_time_log[3:]) * 1000,
-                numpy.mean(total_sampling_time_log[3:]) * 1000))
+    if probs:
+        print(
+            'GPU {} | #Seeds {:.2f} | Indptr host cache rate {:.3f} | Indices host cache rate {:.3f} | Probs host cache rate {:.3f} | #Sampled node {:.1f} | Seeds avg degree {:.2f} | Sampling time {:.3f} ms'
+            .format(rank, numpy.mean(fact_seeds_num_log[3:]),
+                    indptr_fact_host_cache_rate, indices_fact_host_cache_rate,
+                    probs_fact_host_cache_rate,
+                    numpy.mean(minimum_access_node_num_log[3:]),
+                    numpy.mean(avg_seeds_degree_log[3:]),
+                    numpy.mean(sampling_time_log[3:]) * 1000))
+    else:
+        print(
+            'GPU {} | #Seeds {:.2f} | Indptr host cache rate {:.3f} | Indices host cache rate {:.3f} | #Sampled node {:.1f} | Seeds avg degree {:.2f} | Sampling time {:.3f} ms'
+            .format(rank, numpy.mean(fact_seeds_num_log[3:]),
+                    indptr_fact_host_cache_rate, indices_fact_host_cache_rate,
+                    numpy.mean(minimum_access_node_num_log[3:]),
+                    numpy.mean(avg_seeds_degree_log[3:]),
+                    numpy.mean(sampling_time_log[3:]) * 1000))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset",
-                        default="ogbn-papers100M",
+                        default="ogbn-products",
                         choices=[
                             "reddit", "ogbn-products", "ogbn-papers100M",
                             "ogbn-papers400M", "generated"
-                        ],
-                        help="The dataset to be sampled.")
-    parser.add_argument("--root",
-                        default="/data1/",
-                        help="Root path of the dataset.")
+                        ])
+    parser.add_argument("--root", default="/data1/")
+    parser.add_argument('--bias', action='store_true', default=False)
     args = parser.parse_args()
 
     if args.dataset == "reddit":
@@ -134,6 +153,10 @@ if __name__ == '__main__':
 
     indptr = graph.adj_sparse("csc")[0].long()
     indices = graph.adj_sparse("csc")[1].long()
+    if args.bias:
+        probs = torch.rand(graph.num_edges()).float()
+    else:
+        probs = None
     del graph
 
     degree_log = []
@@ -144,7 +167,10 @@ if __name__ == '__main__':
 
     for world_size in [1]:
         print("#GPU = {}".format(world_size))
-        for seeds_num in [1000, 15000, 225000]:
+        for seeds_num in [
+                1000, 2000, 5000, 10000, 15000, 20000, 50000, 100000, 225000
+        ]:
             mp.spawn(bench,
-                     args=(world_size, indptr, indices, 0, 1, [15], seeds_num),
+                     args=(world_size, indptr, indices, probs, 0, 0, 0, [15],
+                           seeds_num),
                      nprocs=world_size)
