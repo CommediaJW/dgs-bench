c1744d99a821208b499a528a27fd5db8c18def6e
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2023-03-26 13:13:31 +0000

    update run_sampling_ncu.sh

--- modeling/run_sampling_ncu.sh
+++ modeling/run_sampling_ncu.sh
@@ -1,7 +1,7 @@
 /usr/local/cuda/bin/ncu --section "regex:MemoryWorkloadAnalysis" -o test -f --nvtx --nvtx-include "sampling]" python3 modeling/sampling_ncu.py 
 
 echo "system read in sector [32 bytes]"
-/usr/local/cuda/bin/ncu -i test.ncu-rep --page raw | grep "lts__t_sectors_srcunit_tex_aperture_sysmem_op_read_lookup_miss.sum"
+/usr/local/cuda/bin/ncu -i test.ncu-rep --page raw | grep "lts__t_sectors_srcunit_tex_aperture_sysmem_op_read_lookup_miss.sum" | grep "sector "
 echo " "
 
 echo "dram read in sector [32 bytes]"
