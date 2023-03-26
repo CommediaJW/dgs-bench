bd6050d945eeb171725f9adc4881ca7f3d0196ef
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2023-03-26 12:50:28 +0000

    add scripts to get host/device memory read/write bytes

---
+++ modeling/run_feature_ncu.sh
@@ -0,0 +1,13 @@
+/usr/local/cuda/bin/ncu --section "regex:MemoryWorkloadAnalysis" -o test -f --nvtx --nvtx-include "loading]" python3 modeling/feature_ncu.py 
+
+echo "system read in sector [32 bytes]"
+/usr/local/cuda/bin/ncu -i test.ncu-rep --page raw | grep "lts__t_sectors_srcunit_tex_aperture_sysmem_op_read_lookup_miss.sum"
+echo " "
+
+echo "dram read in sector [32 bytes]"
+/usr/local/cuda/bin/ncu -i test.ncu-rep --page raw | grep "dram__sectors_read.sum"
+echo " "
+
+echo "dram write in sector [32 bytes]"
+/usr/local/cuda/bin/ncu -i test.ncu-rep --page raw | grep "dram__sectors_write.sum"
+echo " "
